87d2e7dd5d5461bfb472f81e75474547a769e658
nermeenkelila <nermeen_kelila@hotmail.com>
2016-08-10 15:50:58 +0300

    contact view

--- application/views/CMS/Contact.php
+++ application/views/CMS/Contact.php
@@ -37,7 +37,7 @@
               <div class="DataDiv Wide">
                           <div class="DataDiv">
                                <div class="box-wider-text">
-                                    <div class="box-header">
+                                    <!--div class="box-header">
                                         <div class="row CustomRow">
                                         <div class="HeaderLeft"> 
                                         </div>
@@ -45,54 +45,58 @@
                                              <button class="btn btn-sucess" id="AddNewContactShow"> Add New Contact</button>
                                         </div>
                                         </div>
-                                    </div>
+                                    </div-->
                                     <div class="box-body">
-                                       <div class="table-responsive" id="UserTable">
-                                            <table class='table table-hover table-condensed'>
-                                                   <thead>
-                                                        <tr>
-                                                          <th>ID</th>
-                                                          <th>Title</th>
-                                                          <th>Data</th>
-                                                          <th>Edit</th>
-                                                          <th>Delete</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <tr>
-                                                          <td>1</td>
-                                                          <td>WhatsApp</td>
-                                                          <td>039349432</td>
-                                                          <td class='check-col tableAdmin'><a href='#' class='editeBtn' id="EditContactOverlayFormShow" data-placement='right'><span class='fa fa-gear'></span></a></td>
-                                                          <td class='check-col tableAdmin'><a href='#' class='deleteBtn'  data-target='#DeleteContactModal' data-toggle='modal' title='delete' data-placement='right'><span class='fa fa-trash'></span></a></td>
-                                                        </tr> 
-                                                    </tbody>
-                                             </table>
-                                      </div>
+										<div class="FormSection">
+											<form>
+											   <div class="SectionHeader">
+													<h3>Contact Information</h3>
+											   </div>
+												<div class="SectionContent">
+													<div class="form-group formLayout">
+														<label for="ContactTelephone" class="control-label ">Telephone : </label>
+														<input type="text" name="ContactTelephone" class="Contact form-control " placeholder="Telephone" />
+													</div>	
+													<div class="form-group formLayout">
+														<label for="ContactMobile" class="control-label ">Mobile: </label>
+														<input type="text" name="ContactMobile" class="form-control Contact" placeholder="Mobile" />
+													</div>
+													<div class="form-group formLayout">
+														<label for="ContactWhatsApp" class="control-label ">WhatsApp: </label>
+														<input type="text" name="ContactWhatsApp" class="form-control Contact" placeholder="WhatsApp" />
+													</div>	
+													<div class="form-group formLayout">
+														<label for="ContactSkypee" class="control-label ">Skypee: </label>
+														<input type="text" name="ContactSkypee" class="form-control Contact" placeholder="Skypee" />
+													</div>		
+													<div class="form-group formLayout">
+														<label for="ContactYoutube" class="control-label ">Youtube: </label>
+														<input type="text" name="ContactYoutube" class="form-control Contact" placeholder="Youtube" />
+													</div>	
+													<div class="form-group formLayout">
+														<label for="ContactTwitter" class="control-label ">Twitter: </label>
+														<input type="text" name="ContactTwitter" class="form-control Contact" placeholder="Twitter" />
+													</div>
+													<div class="form-group formLayout">
+														<label for="ContactFacebook" class="control-label ">Facebook: </label>
+														<input type="text" name="ContactFacebook" class="form-control Contact" placeholder="Facebook" />
+													</div>
+													<div class="form-group formLayout">
+														<label for="ContactLinkedin" class="control-label ">Linkedin: </label>
+														<input type="text" name="ContactLinkedin" class="form-control Contact" placeholder="Linkedin" />
+													</div>	
+													<div class="form-group formLayout">
+														<label for="ContactAddress" class="control-label ">Address: </label>
+														<input type="text" name="ContactAddress" class="form-control Contact" placeholder="Address" />
+													</div>
+													<div class="form-group formLayout">
+														<button type="button" type="submit" class="btn btn-md OverLayFormBtn submitReply"> Submit</button>
+													</div>
+											</form>
+												</div>
+										</div>
                                     </div>
-                                    <div class="box-footer">
-                                         <div class="row CustomRow">
-                                              <nav>
-                                                <ul class="pagination">
-                                                  <li>
-                                                    <a href="#" aria-label="Previous">
-                                                      <span aria-hidden="true">&laquo;</span>
-                                                    </a>
-                                                  </li>
-                                                  <li><a href="#">1</a></li>
-                                                  <li><a href="#">2</a></li>
-                                                  <li><a href="#">3</a></li>
-                                                  <li><a href="#">4</a></li>
-                                                  <li><a href="#">5</a></li>
-                                                  <li>
-                                                    <a href="#" aria-label="Next">
-                                                      <span aria-hidden="true">&raquo;</span>
-                                                    </a>
-                                                  </li>
-                                                </ul>
-                                              </nav>
-                                         </div>
-                                    </div>
+                         
                                </div>
                           </div>
               </div>
@@ -104,155 +108,7 @@
         <!-----------------------------------------footer------>
         <footer></footer>
 
-        <!-----------------------------------OverLayForms---- -->
-
-        <!----AddNewcontact------------------- -->
-        <div class="OverLayForm closed" id="AddNewContactShowOverLay">
-          <div class="container-fluid OverLayFormHeader">
-               <div class="row CustomRow">
-                   <div class="OverLayFormHeaderLeft">
-                        <h2> Add New Contact </h2>
-                   </div>
-                   <div class="OverLayFormHeaderRight">
-                        <span class="fa fa-close ExitBtn"></span>
-                   </div>
-               </div>
-          </div>
-          <form>
-              <div class="container-fluid OverLayFormContent">
-                   <div class="FormSection">
-                       <div class="SectionHeader">
-                            <h3>Contact Information</h3>
-                       </div>
-                       <div class="SectionContent">
-							<div class="form-group formLayout">
-								<label for="ContactTelephone" class="control-label ">Telephone : </label>
-								<input type="text" name="ContactTelephone" class="form-control " placeholder="Telephone" />
-							</div>	
-							<div class="form-group formLayout">
-								<label for="ContactMobile" class="control-label ">Mobile: </label>
-								<input type="text" name="ContactMobile" class="form-control " placeholder="Mobile" />
-							</div>
-							<div class="form-group formLayout">
-								<label for="ContactWhatsApp" class="control-label ">WhatsApp: </label>
-								<input type="text" name="ContactWhatsApp" class="form-control " placeholder="WhatsApp" />
-							</div>	
-							<div class="form-group formLayout">
-								<label for="ContactSkypee" class="control-label ">Skypee: </label>
-								<input type="text" name="ContactSkypee" class="form-control " placeholder="Skypee" />
-							</div>		
-							<div class="form-group formLayout">
-								<label for="ContactYoutube" class="control-label ">Youtube: </label>
-								<input type="text" name="ContactYoutube" class="form-control " placeholder="Youtube" />
-							</div>	
-							<div class="form-group formLayout">
-								<label for="ContactTwitter" class="control-label ">Twitter: </label>
-								<input type="text" name="ContactTwitter" class="form-control " placeholder="Twitter" />
-							</div>
-							<div class="form-group formLayout">
-								<label for="ContactFacebook" class="control-label ">Facebook: </label>
-								<input type="text" name="ContactFacebook" class="form-control " placeholder="Facebook" />
-							</div>
-							<div class="form-group formLayout">
-								<label for="ContactLinkedin" class="control-label ">Linkedin: </label>
-								<input type="text" name="ContactLinkedin" class="form-control " placeholder="Linkedin" />
-							</div>	
-							<div class="form-group formLayout">
-								<label for="ContactAddress" class="control-label ">Address: </label>
-								<input type="text" name="ContactAddress" class="form-control " placeholder="Address" />
-							</div>
-						
-						
-                      </div>
-                   </div>
-                
-              </div>
-              <div class="container-fluid OverLayFormFooter">
-                   <div class="row CustomRow">
-                       <div class="OverLayFormFooterItem right">
-                            <button type="button"class="btn btn-md OverLayFormBtn"> Creat</button>
-                       </div>
-                       <div class="OverLayFormFooterItem left">
-                       
-                       </div>
-                   </div>
-              </div>
-          </form>
-        </div>
-
-        <!----EditContact------------------- -->
-        <div class="OverLayForm closed" id="EditContactFormShow">
-          <div class="container-fluid OverLayFormHeader">
-               <div class="row CustomRow">
-                   <div class="OverLayFormHeaderLeft">
-                        <h2> Edit Contact </h2>
-                   </div>
-                   <div class="OverLayFormHeaderRight">
-                        <span class="fa fa-close ExitBtn"></span>
-                   </div>
-               </div>
-          </div>
-          <form>
-			<div class="container-fluid OverLayFormContent">
-                   <div class="FormSection">
-                       <div class="SectionHeader">
-                            <h3>Contact Information</h3>
-                       </div>
-                       <div class="SectionContent">
-						<div class="form-group formLayout">
-								<label for="ContactTelephone" class="control-label ">Telephone : </label>
-								<input type="text" name="ContactTelephone" class="form-control " placeholder="Telephone" />
-							</div>	
-							<div class="form-group formLayout">
-								<label for="ContactMobile" class="control-label ">Mobile: </label>
-								<input type="text" name="ContactMobile" class="form-control " placeholder="Mobile" />
-							</div>
-							<div class="form-group formLayout">
-								<label for="ContactWhatsApp" class="control-label ">WhatsApp: </label>
-								<input type="text" name="ContactWhatsApp" class="form-control " placeholder="WhatsApp" />
-							</div>	
-							<div class="form-group formLayout">
-								<label for="ContactSkypee" class="control-label ">Skypee: </label>
-								<input type="text" name="ContactSkypee" class="form-control " placeholder="Skypee" />
-							</div>		
-							<div class="form-group formLayout">
-								<label for="ContactYoutube" class="control-label ">Youtube: </label>
-								<input type="text" name="ContactYoutube" class="form-control " placeholder="Youtube" />
-							</div>	
-							<div class="form-group formLayout">
-								<label for="ContactTwitter" class="control-label ">Twitter: </label>
-								<input type="text" name="ContactTwitter" class="form-control " placeholder="Twitter" />
-							</div>
-							<div class="form-group formLayout">
-								<label for="ContactFacebook" class="control-label ">Facebook: </label>
-								<input type="text" name="ContactFacebook" class="form-control " placeholder="Facebook" />
-							</div>
-							<div class="form-group formLayout">
-								<label for="ContactLinkedin" class="control-label ">Linkedin: </label>
-								<input type="text" name="ContactLinkedin" class="form-control " placeholder="Linkedin" />
-							</div>	
-							<div class="form-group formLayout">
-								<label for="ContactAddress" class="control-label ">Address: </label>
-								<input type="text" name="ContactAddress" class="form-control " placeholder="Address" />
-							</div>
-                      </div>
-                   </div>
-				   
-		    </div> 
-		
-              <div class="container-fluid OverLayFormFooter">
-                   <div class="row CustomRow">
-                       <div class="OverLayFormFooterItem right">
-                            <button type="button"class="btn btn-md OverLayFormBtn"> Creat</button>
-                       </div>
-                       <div class="OverLayFormFooterItem left">
-                       
-                       </div>
-                   </div>
-              </div>
-          </form>
-		  </div>
-        </div>
+      
 
         <!------------------------------------DeleteModals---- -->
         <!----DeleteContact------------------------------>
