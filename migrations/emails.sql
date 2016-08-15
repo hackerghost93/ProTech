292a2d6da5952f4976721890e9fa563ecae1c77b
awd <TheImaginaryKing@gmail.com>
2016-08-15 22:22:49 +0300

    email seen

--- migrations/emails.sql
+++ migrations/emails.sql
@@ -1,30 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Aug 11, 2016 at 01:28 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `protech`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `emails`
---
 
 CREATE TABLE `emails` (
   `id` int(11) NOT NULL,
@@ -35,25 +12,11 @@ CREATE TABLE `emails` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `emails`
---
 ALTER TABLE `emails`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `emails`
---
 ALTER TABLE `emails`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+ALTER TABLE `emails` ADD `Seen` VARCHAR(32) NOT NULL DEFAULT 'new' AFTER `created_at`;
