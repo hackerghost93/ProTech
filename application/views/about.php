42861acd23fd4e72221f263e513f64650c0f3858
yassminmonsif <yassmin.monsif@yahoo.com>
2016-11-18 19:30:03 +0200

    about

--- application/views/about.php
+++ application/views/about.php
@@ -9,11 +9,10 @@
             <div>
                 <h4>عن بروتك</h4>
                 <ul>
-                
-    
-            <p>
-                  لورم ایپسوم أبجد هوز هو مجرد دمية النص من التنضيد والطباعة والصناعة من مصممي الغرافيك. الطابعات والنصوص، ولكن أيضا أعمدة الصحف والمجلات و ما هو ضروري للحال لورم ایپسوم أبجد هوز هو مجرد دمية النص من التنضيد والطباعة والصناعة من مصممي الغرافيك. الطابعات والنصوص، ولكن أيضا أعمدة الصحف والمجلات و ما هو ضروري للحالة </p>
-        
+
+
+                    <p>تأسست شركة بروتك عام 2008 وتعد من اول الشركات الرائدة المتخصصة فى مجال توريد وصيانة ماكينات التصوير XEROX بمحافظة الاسكندرية وبعض المحافظات الاخرى وتضم فريق عمل من ذوى الخبرات الممتازة القادرة على تقديم افضل خدمات الصيانة والتوريد مع اعلى مستوى من الخدمة مما جعلها فى مقدمة الشركات التى كان لها السبق فى التعامل مع كبرى الشركات الموجودة بمختلف المجالات</p>
+
                 </ul>
             </div>
 
@@ -22,17 +21,14 @@
     </section>
     <section class="about2">
         <div class="Grid1k clfx">
-           <span class="a_img img02"> <img src="<?=base_url()?>imgs/about2.png" alt=""></span>
+            <span class="a_img img02"> <img src="<?=base_url()?>imgs/about2.png" alt=""></span>
             <div>
                 <h4>رؤيتنا</h4>
-               
-            <p>
-                  لورم ایپسوم أبجد هوز هو مجرد دمية النص من التنضيد والطباعة والصناعة من مصممي الغرافيك. الطابعات والنصوص، ولكن أيضا أعمدة الصحف والمجلات و ما هو ضروري للحال لورم ایپسوم أبجد هوز هو مجرد دمية النص من التنضيد والطباعة والصناعة من مصممي الغرافيك. الطابعات والنصوص، ولكن أيضا أعمدة الصحف والمجلات و ما هو ضروري للحالة </p>
+                <p> رؤياتنا هي تقديم منتجات وخدمات لبيع و شراء و صيانة الطابعات وفقا لأحدث المعايير الدولية و أن نوفر الميزات الإضافية لعملائنا/p>
+                    <h4>طموحنا</h4>
 
-                <h4>طموحنا</h4>
-            
-            <p>
-                  لورم ایپسوم أبجد هوز هو مجرد دمية النص من التنضيد والطباعة والصناعة من مصممي الغرافيك. الطابعات والنصوص، ولكن أيضا أعمدة الصحف والمجلات و ما هو ضروري للحال لورم ایپسوم أبجد هوز هو مجرد دمية النص من التنضيد والطباعة والصناعة من مصممي الغرافيك. الطابعات والنصوص، ولكن أيضا أعمدة الصحف والمجلات و ما هو ضروري للحالة </p>
+
+                    <p>رؤيتنا هي أن نصبح واحدة من الشركات الرائدة في مجال الطابعات من خلال توفير المنتجات التي تلبي إحتياجات عملائنا</p>
 
             </div>
         </div>
@@ -45,11 +41,12 @@
             <div>
                 <h4>أهدافنا</h4>
                 <ul>
-                
-    
-            <p>
-                  لورم ایپسوم أبجد هوز هو مجرد دمية النص من التنضيد والطباعة والصناعة من مصممي الغرافيك. الطابعات والنصوص، ولكن أيضا أعمدة الصحف والمجلات و ما هو ضروري للحال لورم ایپسوم أبجد هوز هو مجرد دمية النص من التنضيد والطباعة والصناعة من مصممي الغرافيك. الطابعات والنصوص، ولكن أيضا أعمدة الصحف والمجلات و ما هو ضروري للحالة </p>
-        
+
+
+
+                    <p>رؤيتنا هي أن نصبح واحدة من الشركات الرائدة في مجال الطابعات من خلال توفير المنتجات التي تلبي إحتياجات عملائنا</p>
+
+
                 </ul>
             </div>
 
@@ -61,30 +58,30 @@
         <div class="Grid1k clfx">
             <ul>
                 <li class="ix_1">
-                   <div>
-                    <i></i>
-                    <h5>دعم مباشر</h5>
-                   
-<p>نقدم دعم فني حقيقي متواجد يومياً للإجابة علي استفساراتك وحل مشاكل موقعك</p>
-                    
+                    <div>
+                        <i></i>
+                        <h5>دعم مباشر</h5>
+
+                        <p>نقدم دعم فني حقيقي متواجد يومياً للإجابة علي استفساراتك وحل مشاكل موقعك</p>
+
                     </div>
                 </li>
                 <li class="ix_2">
-                   <div>
-                    <i></i>
-                    <h5>تذاكر الدعم</h5>
-                   
-<p>يجيب فريق الدعم الفني علي استفساراتك بخصوص خدماتنا ، حل المشاكل، ومتابعة اداء خدماتنا والتحديثات الدورية</p>
-                  
+                    <div>
+                        <i></i>
+                        <h5>تذاكر الدعم</h5>
+
+                        <p>يجيب فريق الدعم الفني علي استفساراتك بخصوص خدماتنا ، حل المشاكل، ومتابعة اداء خدماتنا والتحديثات الدورية</p>
+
                     </div>
                 </li>
                 <li class="ix_3">
-                   <div>
-                    <i></i>
-                    <h5>عروض مناسبة</h5>
-                  
-<p>نقدم خدمات متنوعة وعروض مناسبة لجميع الأفراد و الشركات بأحدث التقنيات وأفضل جودة</p>
-                    
+                    <div>
+                        <i></i>
+                        <h5>عروض مناسبة</h5>
+
+                        <p>نقدم خدمات متنوعة وعروض مناسبة لجميع الأفراد و الشركات بأحدث التقنيات وأفضل جودة</p>
+
                     </div>
                 </li>
             </ul>
@@ -99,11 +96,9 @@
                 <img src="<?=base_url()?>imgs/img5.png" alt="">
                 <div>
                     <h4>فريق العمل</h4>
-                   
-                    
-            <p>
-                  لورم ایپسوم أبجد هوز هو مجرد دمية النص من التنضيد والطباعة والصناعة من مصممي الغرافيك. الطابعات والنصوص، ولكن أيضا أعمدة الصحف والمجلات و ما هو ضروري للحال لورم ایپسوم أبجد هوز هو مجرد دمية النص من التنضيد والطباعة والصناعة من مصممي الغرافيك. الطابعات والنصوص، ولكن أيضا أعمدة الصحف والمجلات و ما هو ضروري للحالة </p>
-                   
+
+
+                    <p>يتميز فريق عمل بروتك بالقدرة علي الإبتكار حيث التميز في عملهم و لا يتوقفون عن الإبداع و تطوير أنفسهم .كما يوجد لديهم القدرة علي التواصل و تبادل الأراء و النقاش و قادرون علي العمل تحت ضغط دون توقف</p>
 
                 </div>
 
@@ -112,121 +107,125 @@
     </section>
 
 
-    
-<!-- Section 09 -->
-<div class="Section09">
-    <div class="Grid1k">
-        <!-- Contact Area -->
-        <div class="ContactArea clear-after ViewPort">
-            <div class="Center"><img src="<?=base_url()?>imgs/icon26.png" alt=""></div>
-            <div class="Right clear-after">
-                <img src="<?=base_url()?>imgs/icon24.png" alt="">
-                <div class="info">
-                    <div><h3 class="Mobile"></h3>
-                    <h5>متاح واتس اب - فايبر - لاين</h5></div>
+
+    <!-- Section 09 -->
+    <div class="Section09">
+        <div class="Grid1k">
+            <!-- Contact Area -->
+            <div class="ContactArea clear-after ViewPort">
+                <div class="Center"><img src="<?=base_url()?>imgs/icon26.png" alt=""></div>
+                <div class="Right clear-after">
+                    <img src="<?=base_url()?>imgs/icon24.png" alt="">
+                    <div class="info">
+                        <div>
+                            <h3 class="Mobile"></h3>
+                            <h5>متاح واتس اب - فايبر - لاين</h5></div>
+                    </div>
+                </div>
+                <div class="Left clear-after">
+                    <img src="<?=base_url()?>imgs/icon25.png" alt="">
+                    <div class="info">
+                        <h3 class="Email"></h3></div>
                 </div>
             </div>
-            <div class="Left clear-after">
-                <img src="<?=base_url()?>imgs/icon25.png" alt="">
-                <div class="info" ><h3 class="Email"></h3></div>
+            <!-- // Contact Area -->
+        </div>
+    </div>
+    <!-- // Section 09 -->
+
+    <footer>
+        <div class="News-Slider">
+            <div class="Grid1k clear-after">
+                <div class="Title"> احدث العروض</div>
+                <div class="TheSlider">
+
+                </div>
+                <div class="Control clear-after">
+                    <a href="products.html" class="more">المزيد من المنتجات</a>
+                </div>
             </div>
         </div>
-        <!-- // Contact Area -->
-    </div>
-</div>
-<!-- // Section 09 -->
+        <div class="Footer-Container Grid1k">
+            <div class="Contact-Info">
+                <h3>اتصل بنا</h3>
+                <h4 class="Mobile"></h4>
+                <h4 class="Skypee"></h4>
+                <h4 class="WhatsApp"></h4>
+                <h4 class="Telephone"></h4>
+            </div>
+            <div class="SendMail">
+                <h3>القائمة البريدية</h3>
+                <p>سجل بريدك الالكترونى لدينا للحصول على اخر العروض الخاصة المتاحة و اخر اخبار الشركة</p>
 
-   <footer>
-    <div class="News-Slider">
-        <div class="Grid1k clear-after">
-            <div class="Title"> احدث العروض</div>
-            <div class="TheSlider">
-               
-                           </div>
-            <div class="Control clear-after">
-                <a href="products.html" class="more">المزيد من المنتجات</a>
+
+
+                <script type="text/javascript">
+                    //<![CDATA[
+                    if (typeof newsletter_check !== "function") {
+                        window.newsletter_check = function (f) {
+                            var re = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-]{1,})+\.)+([a-zA-Z0-9]{2,})+$/;
+                            if (!re.test(f.elements["email"].value)) {
+                                alert("The email is not correct");
+                                return false;
+                            }
+                            for (var i = 1; i < 20; i++) {
+                                if (f.elements["np" + i] && f.elements["np" + i].required && f.elements["np" + i].value == "") {
+                                    alert("");
+                                    return false;
+                                }
+                            }
+                            if (f.elements["ny"] && !f.elements["ny"].checked) {
+                                alert("You must accept the privacy statement");
+                                return false;
+                            }
+                            return true;
+                        }
+                    }
+                    //]]>
+                </script>
+
+
+                <div class="newsletter newsletter-subscription">
+                    <form class="clear-after" id="subscribeMail" method="post" action="" onsubmit="return newsletter_check(this)">
+
+                        <input class="newsletter-email" type="email" name="email" size="30" placeholder="البريد الإلكتروني" required>
+
+                        <input class="newsletter-submit" type="submit" value="اشترك" />
+
+                    </form>
+                </div>
+
+
+
+
+
+            </div>
+            <div class="SiteInfo">
+                <a href="<?php echo base_url()?>index.php/Email/ViewMassage" class="OpenTicket">فتح تذكرة</a>
+                <div class="Social">
+                    <a target="_blank" href="#" class="Facebook" id="Facebook"><span></span></a>
+                    <a target="_blank" href="#" class="Twitter" id="Twitter"><span></span></a>
+                    <a target="_blank" href="#" class="Youtube" id="Youtube"><span></span></a>
+                    <a target="_blank" href="#" class="LinkedIn" id="Linkedin"><span></span></a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="Footer-Container Grid1k">
-        <div class="Contact-Info">
-            <h3>اتصل بنا</h3>
-            <h4 class="Mobile"></h4>
-            <h4 class="Skypee"></h4>
-            <h4 class="WhatsApp"></h4>
-            <h4 class="Telephone"></h4>
-        </div>
-        <div class="SendMail">
-            <h3>القائمة البريدية</h3>
-            <p>سجل بريدك الالكترونى لدينا للحصول على اخر العروض الخاصة المتاحة و اخر اخبار الشركة</p>
-           
-            
+    </footer>
+    <div class="Copyright clfx">
+        <div class="Grid1k">
+            <span>All Copyright©<a href="http://agedsystems.com/" target="_blank">AGEDSYSTEMS.com</a></span>
+            <ul>
+                <div class="menu-%d8%a8%d8%a8%d8%a8-container">
+                    <ul id="menu-%d8%a8%d8%a8%d8%a8" class="menu">
+                        <li id="menu-item-2470" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2470"><a href="<?php echo base_url()?>index.php/About">عن بروتك</a></li>
 
-<script type="text/javascript">
-//<![CDATA[
-if (typeof newsletter_check !== "function") {
-window.newsletter_check = function (f) {
-    var re = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-]{1,})+\.)+([a-zA-Z0-9]{2,})+$/;
-    if (!re.test(f.elements["email"].value)) {
-        alert("The email is not correct");
-        return false;
-    }
-    for (var i=1; i<20; i++) {
-    if (f.elements["np" + i] && f.elements["np" + i].required && f.elements["np" + i].value == "") {
-        alert("");
-        return false;
-    }
-    }
-    if (f.elements["ny"] && !f.elements["ny"].checked) {
-        alert("You must accept the privacy statement");
-        return false;
-    }
-    return true;
-}
-}
-//]]>
-</script>
-
-
-<div class="newsletter newsletter-subscription">
-    <form class="clear-after" id="subscribeMail" method="post" action="" onsubmit="return newsletter_check(this)">
-
-        <input class="newsletter-email" type="email" name="email" size="30" placeholder="البريد الإلكتروني" required>
-
-        <input class="newsletter-submit" type="submit" value="اشترك"/>
-
-    </form>
-</div>
-            
-            
-            
-            
-            
-        </div>
-        <div class="SiteInfo">
-            <a href="<?php echo base_url()?>index.php/Email/ViewMassage" class="OpenTicket">فتح تذكرة</a>
-            <div class="Social">
-            <a target="_blank" href="#" class="Facebook" id="Facebook"><span></span></a>
-            <a target="_blank" href="#" class="Twitter" id="Twitter"><span></span></a>
-            <a target="_blank" href="#" class="Youtube" id="Youtube"><span></span></a>
-            <a target="_blank" href="#" class="LinkedIn" id="Linkedin"><span></span></a>
-            </div>
+                        <li id="menu-item-2472" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2472"><a href="<?php echo base_url()?>index.php/Email/ViewMassage"> اتصل بنا </a></li>
+                    </ul>
+                </div>
+            </ul>
         </div>
     </div>
-</footer>
-<div class="Copyright clfx">
-    <div class="Grid1k">
-        <span>جميع الحقوق محفوظة لشركة بروتك </span>
-        <ul>
-<div class="menu-%d8%a8%d8%a8%d8%a8-container"><ul id="menu-%d8%a8%d8%a8%d8%a8" class="menu"><li id="menu-item-2470" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2470"><a href="<?php echo base_url()?>index.php/About">عن بروتك</a></li>
-
-<li id="menu-item-2472" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2472"><a href="<?php echo base_url()?>index.php/Email/ViewMassage"> اتصل بنا </a></li>
-</ul>
-            </div>  
-        </ul>
     </div>
-</div>
-</div>
-<div class="big-menu-overly"></div>
+    <div class="big-menu-overly"></div>
 
-<?php $this->load->view('myFooter'); ?>
+    <?php $this->load->view('myFooter'); ?>
